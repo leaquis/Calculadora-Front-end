7e408e8aee52790e1b1207e3bc2131f548a512b2
Giovani da Silva Ramos <gi_1997@hotmail.com>
2025-01-24 02:14:09 -0300

    Fim do projeto

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,71 @@ let operacaoAtual = "";
 let operador = null;
 let valorAnterior = "";
 let calculando = false;
+
+function atualizaDisplay() {
+    display.value = operacaoAtual;
+}
+
+function insereNumero(evento) {
+
+    if (calculando) {
+        operacaoAtual = evento.target.textContent;
+        calculando = false;
+    } else {
+        operacaoAtual += evento.target.textContent;
+    }
+
+    atualizaDisplay();
+}
+
+function inserePonto() {
+    if (operacaoAtual.indexOf(".") === -1) {
+        operacaoAtual += ".";
+        atualizaDisplay();
+    }
+}
+
+function insereOperador(evento) {
+    if (operacaoAtual !== "") {
+        if (!calculando) {
+            if (operador !== null) {
+                calculando();
+            }
+            valorAnterior = operacaoAtual;
+            operacaoAtual = "";
+        }
+        operador = evento.target.textContent;
+    }
+}
+
+function calcula() {
+    let resultado = null;
+    const operandoAnterior = parseFloat(valor);
+    const operandoAtual = parseFloat(operacaoAtual);
+
+    switch (operador) {
+        case "+":
+            resultado = operandoAnterior + operandoAtual
+            break;
+        case "-":
+            resultado = operandoAnterior - operandoAtual
+            break;
+        case "/":
+            resultado = operandoAnterior / operandoAtual
+            break;
+        case "*":
+            resultado = operandoAnterior * operandoAtual
+            break;
+    }
+
+    operacaoAtual = String(resultado);
+    valorAnterior = operacaoAtual;
+    calculando = true;
+    atualizaDisplay();
+}
+
+botaoPonto.addEventListener("click", inserePonto);
+botoesNumeros.forEach((botao) => botao.addEventListener("click", insereNumero));
+botoesOperadores.forEach((botao) => botao.addEventListener("click", insereOperador));
+
+botaoIgual.addEventListener("click", calcula)
